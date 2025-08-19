c5caee2276c9b04d7aa36534db6f9ff6dec23940
Aravinth S <aravinthsaravanan4285@gmail.com>
2025-08-19 10:05:19 +0530

    Module Caching

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-require("./batman");
-require("./superman");
+const superHero = require("./super-hero");
+console.log(superHero.getName());
+superHero.setName("Superman");
+console.log(superHero.getName());
+
+const newSuperHero = require("./super-hero");
+console.log(newSuperHero.getName());
+
+/*
+Output:
+Batman
+Superman
+Superman - due to Module caching (node cache the require("./super-hero") and objects are passed by reference) 
+*/
