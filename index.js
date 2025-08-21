5511885966af9c2855ff119016c6ea4dbf5bf7f1
Aravinth S <aravinthsaravanan4285@gmail.com>
2025-08-21 08:25:26 +0530

    node:fs/promises

--- index.js
+++ index.js
@@ -1,31 +1,48 @@
-const fs = require("node:fs");
+const fs = require("node:fs/promises");
 
-const fileContents = fs.readFileSync("./file.txt");
-console.log(fileContents);
-// <Buffer 54 68 69 73 20 69 73 20 61 20 66 69 6c 65 2e>
+fs.readFile("file.txt", "utf-8")
+.then(data => console.log(data))
+.catch(error => console.log(error));
 
-const fileContents1 = fs.readFileSync("./file.txt", "utf-8");
-console.log(fileContents1);
-// This is a file.
-
-fs.readFile("./file.txt", (error, data) => {
-  if(error){
+async function readFile() {
+  try{
+    const data = await fs.readFile("file.txt", "utf-8");
+  }
+  catch(error){
     console.log(error);
   }
-  else{
-    console.log(data);
-    // <Buffer 54 68 69 73 20 69 73 20 61 20 66 69 6c 65 2e>
-  }
-});
+};
 
-// Synchronous
-fs.writeFileSync("./greet.txt", "Hello Ethereum!");
-// Asynchronous
-fs.writeFileSync("./greet.txt", " Hello Aave!", { flag: "a"}, (error) => {
-  if(error){
-    console.log(error);
-  }
-  else{
-    console.log("File written successfully");
-  }
-});
+readFile();
+
+// const fs = require("node:fs");
+
+// const fileContents = fs.readFileSync("./file.txt");
+// console.log(fileContents);
+// // <Buffer 54 68 69 73 20 69 73 20 61 20 66 69 6c 65 2e>
+
+// const fileContents1 = fs.readFileSync("./file.txt", "utf-8");
+// console.log(fileContents1);
+// // This is a file.
+
+// fs.readFile("./file.txt", (error, data) => {
+//   if(error){
+//     console.log(error);
+//   }
+//   else{
+//     console.log(data);
+//     // <Buffer 54 68 69 73 20 69 73 20 61 20 66 69 6c 65 2e>
+//   }
+// });
+
+// // Synchronous
+// fs.writeFileSync("./greet.txt", "Hello Ethereum!");
+// // Asynchronous
+// fs.writeFileSync("./greet.txt", " Hello Aave!", { flag: "a"}, (error) => {
+//   if(error){
+//     console.log(error);
+//   }
+//   else{
+//     console.log("File written successfully");
+//   }
+// });
