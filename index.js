16a88275d5f833afe3fd85fe59fbf7ce788eae89
Aravinth S <aravinthsaravanan4285@gmail.com>
2025-08-21 08:34:15 +0530

    Stream using node:fs

--- index.js
+++ index.js
@@ -1,48 +1,54 @@
-const fs = require("node:fs/promises");
+const fs = require("node:fs");
 
-fs.readFile("file.txt", "utf-8")
-.then(data => console.log(data))
-.catch(error => console.log(error));
+const readableStream = fs.createReadStream("./file.txt", {
+  encoding: "utf-8",
+  highWaterMark: 2,
+});
 
-async function readFile() {
-  try{
-    const data = await fs.readFile("file.txt", "utf-8");
-  }
-  catch(error){
-    console.log(error);
-  }
-};
+const writeableStream = fs.createWriteStream("./file2.txt");
 
-readFile();
+readableStream.on("data", (chunk) => {
+  console.log(chunk);
+  writeableStream.write(chunk);
+});
 
-// const fs = require("node:fs");
-
-// const fileContents = fs.readFileSync("./file.txt");
-// console.log(fileContents);
-// // <Buffer 54 68 69 73 20 69 73 20 61 20 66 69 6c 65 2e>
-
-// const fileContents1 = fs.readFileSync("./file.txt", "utf-8");
-// console.log(fileContents1);
-// // This is a file.
-
-// fs.readFile("./file.txt", (error, data) => {
-//   if(error){
-//     console.log(error);
-//   }
-//   else{
-//     console.log(data);
-//     // <Buffer 54 68 69 73 20 69 73 20 61 20 66 69 6c 65 2e>
-//   }
-// });
-
-// // Synchronous
-// fs.writeFileSync("./greet.txt", "Hello Ethereum!");
-// // Asynchronous
-// fs.writeFileSync("./greet.txt", " Hello Aave!", { flag: "a"}, (error) => {
-//   if(error){
-//     console.log(error);
-//   }
-//   else{
-//     console.log("File written successfully");
-//   }
-// });
+// for highWaterMark: 2, the output be like
+/*
+Et
+he
+re
+um
+ i
+s 
+a 
+de
+ce
+nt
+ra
+li
+ze
+d 
+bl
+oc
+kc
+ha
+in
+ w
+it
+h 
+sm
+ar
+t 
+co
+nt
+ra
+ct
+ f
+un
+ct
+io
+na
+li
+ty
+.
+*/
