5184f3a5d266dbd44a4fa656f62377f474f3ccd5
Aravinth S <aravinthsaravanan4285@gmail.com>
2025-08-21 08:57:15 +0530

    Create a Node Server

--- index.js
+++ index.js
@@ -1,76 +1,15 @@
-/*
-3. Types of Streams
+const http = require("node:http");
 
-Node.js defines four main stream types:
+const server = http.createServer((req, res) => {
+  const superProtocol = {
+    firstName: "Ethereum",
+    lastName: "Digital Ledger",
+  };
 
-Readable Streams → data can be read (fs.createReadStream, http.IncomingMessage)
-
-Writable Streams → data can be written (fs.createWriteStream, http.ServerResponse)
-
-Duplex Streams → both readable and writable (net.Socket)
-
-Transform Streams → duplex + data can be modified as it passes through (zlib.createGzip)
-*/
-
-const fs = require("node:fs");
-const zlib = require("node:zlib");
-
-const gzip = zlib.createGzip();
-
-const readableStream = fs.createReadStream("./file.txt", {
-  encoding: "utf-8",
-  highWaterMark: 2,
+  res.writeHead(200, {"Content-Type": "application/json"});
+  res.end(JSON.stringify(superProtocol));
 });
 
-// readable to transformable, and then to writeable stream
-readableStream.pipe(gzip).pipe(fs.WriteStream("./file2.txt.gz"));
-
-const writeableStream = fs.createWriteStream("./file2.txt");
-
-readableStream.pipe(writeableStream);
-
-/*readableStream.on("data", (chunk) => {
-  console.log(chunk);
-  writeableStream.write(chunk);
-});*/
-
-// for highWaterMark: 2, the output be like
-/*
-Et
-he
-re
-um
- i
-s 
-a 
-de
-ce
-nt
-ra
-li
-ze
-d 
-bl
-oc
-kc
-ha
-in
- w
-it
-h 
-sm
-ar
-t 
-co
-nt
-ra
-ct
- f
-un
-ct
-io
-na
-li
-ty
-.
-*/
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+})
