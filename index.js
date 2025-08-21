01a337c09e6c3d0d0e52f454177acd6437a876e5
Aravinth S <aravinthsaravanan4285@gmail.com>
2025-08-21 08:42:16 +0530

    Types of Streams and examples

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
+/*
+3. Types of Streams
+
+Node.js defines four main stream types:
+
+Readable Streams → data can be read (fs.createReadStream, http.IncomingMessage)
+
+Writable Streams → data can be written (fs.createWriteStream, http.ServerResponse)
+
+Duplex Streams → both readable and writable (net.Socket)
+
+Transform Streams → duplex + data can be modified as it passes through (zlib.createGzip)
+*/
+
 const fs = require("node:fs");
+const zlib = require("node:zlib");
+
+const gzip = zlib.createGzip();
 
 const readableStream = fs.createReadStream("./file.txt", {
   encoding: "utf-8",
   highWaterMark: 2,
 });
 
+// readable to transformable, and then to writeable stream
+readableStream.pipe(gzip).pipe(fs.WriteStream("./file2.txt.gz"));
+
 const writeableStream = fs.createWriteStream("./file2.txt");
 
-readableStream.on("data", (chunk) => {
+readableStream.pipe(writeableStream);
+
+/*readableStream.on("data", (chunk) => {
   console.log(chunk);
   writeableStream.write(chunk);
-});
+});*/
 
 // for highWaterMark: 2, the output be like
 /*
