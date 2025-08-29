3a21fe1025578528de8dc0ea361b3a76ce12d71b
Aravinth S <aravinthsaravanan4285@gmail.com>
2025-08-29 19:13:14 +0530

    Middleware

---
+++ express/middleware.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const myFirstMiddleWare = (req, res, next) => {
+    console.log("This is the first middleware which will run on every request");
+    next();
+};
+
+app.use(myFirstMiddleWare);
+
+app.get('/', (req, res) => {
+    res.send("Home Page");
+});
+
+app.get('/about', (req, res) => {
+    res.send("About Page");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at port ${port}`);
+});
