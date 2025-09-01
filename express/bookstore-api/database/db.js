67da2e7bf9595fcde2fa4b25f5e5448d5b71dd6b
Aravinth S <aravinthsaravanan4285@gmail.com>
2025-09-01 10:10:22 +0530

    Bookstore API

---
+++ express/bookstore-api/database/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const connectToDB = async() => {
+    try{
+        await mongoose.connect('mongodb+srv://aravinth_in:<password>@cluster0.j5liqng.mongodb.net/bookstore-api')
+        console.log('Database is connected successfully');
+    }catch(error){
+        console.error('Database connection is failed', error);
+        process.exit(1);
+    }
+}
+
+module.exports = connectToDB;
---
+++ express/bookstore-api/server.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require("express");
+const connectToDB = require('./database/db');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Connect to database
+connectToDB();
+
+// Middleware
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`);
+});
+
