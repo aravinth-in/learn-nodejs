19a112acaef36647f501fe18fd38f9f978b03991
Aravinth S <aravinthsaravanan4285@gmail.com>
2025-09-06 19:15:43 +0530

    Auth - Sorting & Pagination

--- express/auth/controllers/image-controller.js
+++ express/auth/controllers/image-controller.js
@@ -1,6 +1,6 @@
 const Image = require('../models/Image');
 const {uploadToCloudinary} = require('../helpers/cloudinaryHelper');
-const { model } = require('mongoose');
+const cloudinary = require('../config/cloudinary');
 
 const uploadImageController = async(req, res) => {
     try {
@@ -33,6 +33,80 @@ const uploadImageController = async(req, res) => {
     }
 };
 
+
+const fetchImagesController = async(req, res) => {
+    try {
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 5;
+        const skip = (page - 1) * limit;
+
+        const sortBy = req.query.sortBy || 'createdAt';
+        const sortOrder = req.query.sortOrder === 'asc' ? 1 : -1;
+        const totalImages = await Image.countDocuments();
+        const totalPages = Math.ceil(totalImages/limit);
+
+        const sortObj = {};
+        sortObj[sortBy] = sortOrder; // { createdAt: -1 }
+        const images = await Image.find().sort(sortObj).skip(skip).limit(limit);
+
+        if(images){
+            return res.status(200).json({
+                success : true,
+                currentPage : page,
+                totalPages : totalPages,
+                totalImages : totalImages,
+                data : images
+            });
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success : false,
+            message : 'Something went wrong! Please try again'
+        })
+    }
+};
+
+
+const deleteImageController = async(req, res) => {
+    try {
+        const imageId = req.params.id;
+        const userId = req.userInfo.userId;
+        const image = await Image.findById(imageId);
+        if(!image){
+            return res.status(404).json({
+                success : false,
+                message : 'Image not found',
+                imageid : imageId
+            });
+        }
+
+        if(image.uploadedBy.toString() !== userId){
+            return res.status(403).json({
+                success : false,
+                message : 'You are not authorized to delete this image.',
+            });
+        }
+
+        await cloudinary.uploader.destroy(image.publicId);
+        await Image.findByIdAndDelete(imageId);
+
+        res.status(200).json({
+            success : true,
+            message : 'Image is deleted successfully'
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success : false,
+            message : 'Something went wrong! Please try again'
+        })
+    }
+};
+
 module.exports = {
-    uploadImageController
+    uploadImageController,
+    fetchImagesController,
+    deleteImageController,
 };
--- express/auth/routes/image-routes.js
+++ express/auth/routes/image-routes.js
@@ -1,18 +1,23 @@
-const express = require('express');
-const authMiddelware = require('../middleware/auth-middleware');
-const adminMiddelware = require('../middleware/admin-middleware');
-const uploadMiddleware = require('../middleware/upload-middleware');
-const { uploadImageController } = require('../controllers/image-controller');
+const express = require("express");
+const authMiddelware = require("../middleware/auth-middleware");
+const adminMiddelware = require("../middleware/admin-middleware");
+const uploadMiddleware = require("../middleware/upload-middleware");
+const {
+  uploadImageController,
+  fetchImagesController,
+  deleteImageController,
+} = require("../controllers/image-controller");
 
 const router = express.Router();
 
 router.post(
-    '/upload',
-    authMiddelware,
-    adminMiddelware,
-    uploadMiddleware.single('image'),
-    uploadImageController
+  "/upload",
+  authMiddelware,
+  adminMiddelware,
+  uploadMiddleware.single("image"),
+  uploadImageController
 );
-
+router.get("/get", authMiddelware, fetchImagesController);
+router.delete('/:id', authMiddelware, adminMiddelware, deleteImageController);
 
 module.exports = router;
