fe99bfa029d6b7fd16a0edcbbf9041c1ad3d2872
Aravinth S <aravinthsaravanan4285@gmail.com>
2025-09-08 09:02:51 +0530

    MongoDB - Aggregation

---
+++ express/mongoose/server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const productRoutes = require("./routes/product-routes");
+
+const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Database is connected successfully"))
+  .catch((e) => console.log(e));
+
+app.use(express.json());
+
+app.use("/products", productRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running at port ${process.env.PORT}`);
+});
